6961bb4b8141a1e0400d7fc49632806a3e3cf04c
Leonardo Tomé <30002511@students.ual.pt>
2019-12-22 16:21:55 +0000

    Program.java switch cases completed

--- src/healthunit/app/Program.java
+++ src/healthunit/app/Program.java
@@ -30,41 +30,64 @@ public class Program {
 				healthunit.createFamily(commands[1]);
 				break;
 			case "AF":
+				healthunit.associateCustomertoFamily(commands[1], commands[2]);
 				break;
 			case "DF":
+				healthunit.deassociateCustomertoFamily(commands[1], commands[2]);
 				//Desassociar utente de família
 				break;
 			case "LP":
 				healthunit.listEmployees();
+				//Listar profissionais
 				break;
 			case "LU":
+				healthunit.listCustomers();
 				//Listar utentes
 				break;
 			case "LF":
+				healthunit.listFamilies();
 				//Listar famílias
 				break;
 			case "MF":
+				healthunit.showFamily(commands[1]);
 				//Mostrar familia
 				break;
 			case "MC":
+				healthunit.createAppointment(commands[1]);
 				//Marcar cuidados a utente
 				break;
 			case "LCU":
+				healthunit.showAppointmentstoCustomer(commands[1]);
 				//Listar cuidados marcados a utente
 				break;
 			case "LCF":
+				healthunit.showAppointmentstoFamily(commands[1]);
 				//Listar cuidados marcados a família
 				break;
 			case "LSP":
+				healthunit.showAppointmentstoEmployee(commands[1], commands[2]);
 				//Listar serviços marcados a profissional
 				break;
 			case "LMS":
+				healthunit.showAppointmentsperService(commands[1]);
 				//Listar marcações por tipo de serviço
 				break;
 			case "G":
+				boolean stateSave = healthunit.save();
+				if(stateSave == true) {
+					System.out.println("Unidade de saúde gravada.");
+				} else {
+					System.out.println("Ocorreu um erro na gravação.");
+				}
 				//Gravar 
 				break;
 			case "L":
+				boolean stateRead = healthunit.load();
+				if(stateRead == true) {
+					System.out.println("Unidade de saúde carregada.");
+				} else {
+					System.out.println("Ocorreu um erro no carregamento.");
+				}
 				//Ler
 				break;
 			default:
